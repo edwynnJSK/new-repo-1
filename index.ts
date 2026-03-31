9728b50d773742fb1f588d744ecb52e9ef005578
edwynnJSK <jose35lert@hotmail.com>
2026-03-31 11:53:49 -0500

    feature: new instance for auto interface

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 const carTypes = {
-    arc: "1",
-    arc2: "2",
-    arc3: "3"
+  arc: "1",
+  arc2: "2",
+  arc3: "3",
 } as const;
 
 const kev: keyof typeof carTypes = "arc";
@@ -9,10 +9,21 @@ const kev: keyof typeof carTypes = "arc";
 console.log("Type of car: " + carTypes[kev]);
 
 interface Auto {
-    id: number,
-    brand: string,
-    model: string,
-    motor: string,
-    color: string,
-    showBrand: () => void;
+  id: number;
+  brand: string;
+  model: string;
+  motor: string;
+  color: string;
+  showBrand: () => void;
 }
+
+const auto: Auto = {
+  id: 1,
+  brand: "Ferrari",
+  color: "Red",
+  model: "Enzo",
+  motor: "V12",
+  showBrand: () => console.log(`Brand is: ${auto.brand}`)
+};
+
+auto.showBrand();
