4b231246e1e7d76d5e0e7d439f765b2e37ac1a95
edwynnJSK <jose35lert@hotmail.com>
2026-04-13 22:34:04 -0500

    fix: resolved merge with main branch

--- index.ts
+++ index.ts
@@ -8,20 +8,11 @@ const kev: keyof typeof carTypes = "arc";
 
 console.log("Type of car: " + carTypes[kev]);
 
-interface User {
+interface Auto {
     id: number,
-    name: string,
-    telephone: string,
-    isActive: boolean;
-    calc: () => void;
+    brand: string,
+    model: string,
+    motor: string,
+    color: string,
+    showBrand: () => void;
 }
-
-const user: User = {
-    id: 2,
-    name: "User1",
-    telephone: "1234567890",
-    isActive: true,
-    calc: () => console.log("new Func")
-}
-
-user.calc();
